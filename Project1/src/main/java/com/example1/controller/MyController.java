856270d8b2920b7d1ed241f03706047fe1d94301
Abhishekp2917 <abhishekp2917@gmail.com>
2024-01-21 12:43:34 +0530

    Added Project1

---
+++ Project1/src/main/java/com/example1/controller/MyController.java
@@ -0,0 +1,44 @@
+package com.example1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class MyController {
+
+    @GetMapping("/")
+    public String getHome() {
+
+        return "Welcome to Home Page";
+    }
+
+    @GetMapping("/myAccount")
+    public String getMyAccount() {
+
+        return "Welcome to your Account Page";
+    }
+
+    @GetMapping("/myLoans")
+    public String getMyLoans() {
+
+        return "Welcome to your Loans Page";
+    }
+
+    @GetMapping("/myCards")
+    public String getMyCards() {
+
+        return "Welcome to your Cards Page";
+    }
+
+    @GetMapping("/notices")
+    public String getNotices() {
+
+        return "Welcome to Notice Page";
+    }
+
+    @GetMapping("/contact")
+    public String getContact() {
+
+        return "Welcome to Contact Page";
+    }
+}
